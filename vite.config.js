fcdfcb3654b11f424b6a3be269f3182c7ed2db0c
HUANDAANDRIELLY <huandaandrielly@gmail.com>
2026-09-22 14:23:02 -0400

    Set base path for project in Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+    base: "/projeto-ong-maos-que-transformam/",
     build: {
         rollupOptions: {
             input: "html/index.html"
